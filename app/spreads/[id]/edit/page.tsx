4c00dcb36890568e7ef7490fe21eab13cde74c0a
McKenney Leavell <maleavell@gmail.com>
2026-01-13 15:46:07 -0600

    Title required to save new and existing spreads

--- app/spreads/[id]/edit/page.tsx
+++ app/spreads/[id]/edit/page.tsx
@@ -9,6 +9,7 @@ export default function EditSpread({ params }) {
     const paramString = React.use(params)
     const id = paramString.id
     const [spread, setSpread] = useState({})
+    const [toggleRequiredFieldMsg, setToggleRequiredFieldMsg] = useState(false)
     const titleEl = useRef()
     const interpretationEl = useRef()
     const router = useRouter() 
@@ -28,11 +29,16 @@ export default function EditSpread({ params }) {
     }, [titleEl, interpretationEl, spread])
 
     const saveSpread = () => {
-        updateSpread(id, {
-            title: titleEl.current.value,
-            interpretation: interpretationEl.current.value
-        })
-        .then(router.push('/spreads'))
+        if (titleEl.current.value === " " || titleEl.current.value === "") {
+            setToggleRequiredFieldMsg(true)
+            return console.log("No title added")
+        } else {
+            updateSpread(id, {
+                title: titleEl.current.value,
+                interpretation: interpretationEl.current.value
+            })
+            .then(router.push('/spreads'))
+        }
     }
 
 
@@ -41,7 +47,7 @@ export default function EditSpread({ params }) {
             <div className="min-h-screen items-center justify-center bg-(--clr-surface-tonal-a10)">
                 <main className="m-auto min-h-screen max-w-3xl flex-col items-center py-32 px-16">
                     <h1 className="justify-self-center max-w-xs text-3xl font-bold leading-10 tracking-tight p-4 text-(--clr-light-a0)">Edit Spread</h1>
-                    <SpreadForm saveSpread={saveSpread} titleEl={titleEl} interpretationEl={interpretationEl} spread={spread} />
+                    <SpreadForm saveSpread={saveSpread} titleEl={titleEl} interpretationEl={interpretationEl} spread={spread} toggleFieldMsg={toggleRequiredFieldMsg} />
                 </main>
             </div>
         </>
